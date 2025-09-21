1552072780cd158c196a0a1b73316ba7dde57e67
Clare Yong <claremagdelene@gmail.com>
2025-09-22 00:52:51 +0800

    add frog

---
+++ sept-21/frog.py
@@ -0,0 +1,21 @@
+"""
+There are n stones, each with height. Height difference incurs cost.
+Frog will jump one stone to another. He can jump to next stone, or skip it. 
+Find minimum cost frog can jump from one stone to another.
+
+E.g. [10, 30, 40, 20]
+"""
+
+n = int(input())
+jumps = list(map(int, input().split()))
+
+prev = 0
+curr = abs(jumps[1] - jumps[0]) # [0, 20, 10, ]
+
+for i in range(2, n):
+    curr_stone = jumps[i]
+    jump = abs(curr_stone - jumps[i-1]) + curr # 20
+    skip = abs(curr_stone - jumps[i-2]) + prev
+    prev = curr
+    curr = min(jump, skip)
+print(curr)
